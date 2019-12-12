bbaee8ca8738139b9f505bdea2382268316d0875
RuslanGlagolev <58308436+RuslanGlagolev@users.noreply.github.com>
2019-12-12 18:31:20 +0200

    Remove_Glagolev_Ruslan_homework1

--- HomeWork1/Glagolev_Ruslan_homework1.cpp
+++ HomeWork1/Glagolev_Ruslan_homework1.cpp
@@ -16,31 +16,45 @@ int main() {
   double kx2 = (-kb - sqrt(kd)) / (2 * ka);
   std::cout << "First root=" << kx1 << std::endl;
   std::cout << "Second root=" << kx2 << std::endl;
+  int min = -255;
+  int max = 255;
   std::cout
       << "This program calculates the roots of a quadratic equation of type:\n"
       << "a*x^2+b*x+c \n"
       << std::endl;
   int a;
-  int b;
-  int c;
-  std::cout << "Enter a" << std::endl;
+  std::cout << "Enter the number 'a', in the range -255 to 255" << std::endl;
   std::cin >> a;
-  while (a == 0 || a > 255 || a < -255) {
+  while (a == 0 || a > max || a < min) {
     std::cout << "Not correct number \n"
               << "Enter a" << std::endl;
     std::cin >> a;
   }
-  std::cout << "Enter b" << std::endl;
+  int b;
+  std::cout << "Enter the number 'b', in the range -255 to 255" << std::endl;
   std::cin >> b;
-  std::cout << "Enter c" << std::endl;
+  while (b == 0 || b > max || b < min) {
+    std::cout << "Not correct number \n"
+              << "Enter a" << std::endl;
+    std::cin >> b;
+  }
+  int c;
+  std::cout << "Enter the number 'c', in the range -255 to 255" << std::endl;
   std::cin >> c;
-  int d;
-  d = b * b - 4 * a * c;
+  while (c == 0 || c > max || c < min) {
+    std::cout << "Not correct number \n"
+              << "Enter a" << std::endl;
+    std::cin >> c;
+  }
+
+  auto d = b * b - 4 * a * c;
   if (d > 0) {
     double x1 = (-b + sqrt(d)) / (2 * a);
     std::cout << "First root \n" << x1 << std::endl;
     double x2 = (-b - sqrt(d)) / (2 * a);
     std::cout << "Second root \n" << x2;
+  } else {
+    std::cout << "Not root"
   }
   return 0;
 }
