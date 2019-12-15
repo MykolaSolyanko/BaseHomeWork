9d454f99cf647e9f2de937221a956156340530a5
RuslanGlagolev <58308436+RuslanGlagolev@users.noreply.github.com>
2019-12-15 17:06:50 +0200

    Resolve_Glagolev_Ruslan_homework2_task1

--- HomeWork2/Glagolev_Ruslan_task1.cpp
+++ HomeWork2/Glagolev_Ruslan_task1.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 
 int main() {
-  int n;
+  int number;
   int sum = 0;
-  int i = 0;
+  int count_digit_in_number = 0;
 
-  std::cout << "please, enter n = ";
-  std::cin >> n;
+  std::cout << "Please enter any number, this program will calculate the sum "
+               "of the\n";
+  std::cout << "digits in the number, and Arithmetic mean of the digits of "
+               "your number-";
+  std::cin >> number;
 
-  while (n != 0) {
-    sum += n % 10;
-    n /= 10;
-    i++;
+  while (number != 0) {
+    sum += number % 10;
+    number /= 10;
+    count_digit_in_number++;
   }
-  double avarage = (double)sum / (double)i;
-  std::cout << "sum = " << sum << std::endl;
-  std::cout << "avarage = " << avarage << std::endl;
+  auto avarage = static_cast<double>(sum) / count_digit_in_number;
+  std::cout << "The sum of the digits of your number- " << sum << std::endl;
+  std::cout << "Arithmetic mean of the digits of your number - " << avarage
+            << std::endl;
 
   return 0;
 }
