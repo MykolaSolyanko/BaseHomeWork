585db475f42e3fe623e3b3bd0ad0d987cf737964
RuslanGlagolev <58308436+RuslanGlagolev@users.noreply.github.com>
2019-12-15 18:34:53 +0200

    Create_Glagolev_Ruslan_homework2_task6

---
+++ HomeWork2/Glagolev_Ruslan_task6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main()
+{
+int number;
+int b = 0;
+std::cout << "This program draws a Christmas tree with the symbol '*', which will be based on the number you specify.\n";
+std::cout<<"You need to enter only odd numbers from 0 to 100."<<std::endl;
+std::cout << "Enter your number-";
+std::cin >> number;
+while (number % 2 == 0|| number <0|| number>100)
+{
+std::cout << "Enter the correct number- ";
+std::cin >> number;
+}
+for (int a = 0; a != (number + 1) / 2; a++) {
+for (int i = 0; i < number; i++)
+{
+int max = i + b;
+int min = i - b;
+if (max >= number / 2 && min <= number / 2) {
+std::cout << "*";
+}
+else {
+std::cout << " ";
+}
+}
+std::cout << "\n";
+b++;
+}
+return 0;
+}
