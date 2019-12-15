896028a37a516fb623ef9109bc5aecb8a58dd2d6
RuslanGlagolev <58308436+RuslanGlagolev@users.noreply.github.com>
2019-12-15 17:14:13 +0200

    Glagolev_Ruslan_homework2_task5

---
+++ HomeWork2/Glagolev_Ruslan_task5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+long long number;
+long long best_divisor=0;
+std::cout << "This program calculates the best divisor of your number.\n";
+std::cout << "Enter tour number- ";
+std::cin >> number;
+for (long long i = 1; i <= number; i++){
+if (number% i == 0&&i!=number){
+std::cout << "Divisor- " << i << "\n";
+if (best_divisor<i){
+best_divisor = i;}
+}
+}
+std::cout << "The best divisor your number- " << best_divisor;
+return 0;
+}
