2d51815f7f498531e084381e4f8b59630c842c8f
Rafig Rzayev <rafigrzayev7@gmail.com>
2020-01-03 22:25:58 +0200

    RafigRzayev_HW4.cpp v2

--- HomeWork4/RafigRzayev_HW4.cpp
+++ HomeWork4/RafigRzayev_HW4.cpp
@@ -4,24 +4,25 @@
 #include <limits>
 #include <random>
 
-using comp = std::function<bool(int, int)>;
-using comp_double = std::function<bool(double, double)>;
+using Comp = std::function<bool(long, long)>;
+using CompDouble = std::function<bool(double, double)>;
+using CharSettings = std::function<bool(char)>;
 
 void clear_stream() {
   std::cin.clear();
   std::cin.ignore();
 }
 
-void swap(int *p1, int *p2) {
-  int temp = *p2;
-  *p2 = *p1;
-  *p1 = temp;
+void swap(int &a, int &b) {
+  int temp = b;
+  b = a;
+  a = temp;
 }
 
-void swap(char *p1, char *p2) {
-  char temp = *p2;
-  *p2 = *p1;
-  *p1 = temp;
+void swap(char &a, char &b) {
+  char temp = b;
+  b = a;
+  a = temp;
 }
 
 /***************************** PRINT ARRAYS (<INT>, <DOUBLE>, <UNSIGNED>, <CHAR>) ******************************/
@@ -63,8 +64,8 @@ void print_sequence(const unsigned *begin, const unsigned *const end) {
 }
 
 // prints sequence of char
-void print_sequence(const char *begin, const char *const end) {
-  if (begin == nullptr || end == nullptr) {
+void print_sequence(const char *begin) {
+  if (begin == nullptr) {
     std::cout << "print_sequence() - Exception\n";
     return;
   }
@@ -126,7 +127,7 @@ int random_number(const size_t N) {
 /********************************** BUBBLE SORT for <INT>, <DOUBLE>, <UNSIGNED> *************************************/
 
 // sorts an integer sequence according to the bubble sort algorithm
-void bubble_sort(int *begin, int *const end, comp predicate) {
+void bubble_sort(int *begin, int *const end, Comp predicate) {
   if (begin == nullptr || end == nullptr) {
     std::cout << "bubble_sort() - Exception\n";
     return;
@@ -136,7 +137,7 @@ void bubble_sort(int *begin, int *const end, comp predicate) {
     size_t swap_counter{0};
     while (it != end - 1) {
       if (predicate(*it, *(it + 1))) {
-        swap(it, it + 1);
+        swap(*it, *(it + 1));
         ++swap_counter;
       }
       ++it;
@@ -148,7 +149,7 @@ void bubble_sort(int *begin, int *const end, comp predicate) {
 }
 
 // sorts a double sequence according to the bubble sort algorithm
-void bubble_sort(double *begin, const double *const end, comp_double predicate) {
+void bubble_sort(double *begin, const double *const end, CompDouble predicate) {
   if (begin == nullptr || end == nullptr) {
     std::cout << "bubble_sort() - Exception\n";
     return;
@@ -172,7 +173,7 @@ void bubble_sort(double *begin, const double *const end, comp_double predicate) 
 }
 
 // sorts an unsigned sequence according to the bubble sort algorithm
-void bubble_sort(unsigned *begin, const unsigned *const end, comp predicate) {
+void bubble_sort(unsigned *begin, const unsigned *const end, Comp predicate) {
   if (begin == nullptr || end == nullptr) {
     std::cout << "bubble_sort() - Exception\n";
     return;
@@ -212,34 +213,18 @@ char *find_null_terminator(char *begin, char *end) {
   return end;
 }
 
-// returns address of first digit in char sequence
-char *find_first_digit(char *&begin, char *end) {
+char *find_first_occurence(char * begin, char *end, CharSettings predicate) {
   if (begin == nullptr || end == nullptr) {
-    std::cout << "find_first_digit() - Exception\n";
+    std::cout << "find_first_occurence() - Exception\n";
     return end;
   }
   while (*begin != '\0') {
-    if (*begin >= '0' && *begin <= '9') {
+    if (predicate(*begin)) {
       return begin;
     }
     ++begin;
   }
-  return end;
-}
-
-// returns address of first non-digit in char sequence
-char *find_first_non_digit(char *&begin, char *end) {
-  if (begin == nullptr || end == nullptr) {
-    std::cout << "find_first_non_digit() - Exception\n";
-    return end;
-  }
-  while (*begin != '\0') {
-    if (*begin < '0' || *begin > '9') {
-      return begin;
-    }
-    ++begin;
-  }
-  return end;
+  return end;  
 }
 
 // moves digits to the end of char sequence
@@ -249,15 +234,20 @@ void shift(char *begin, char *end) {
     return;
   }
   char *it = begin;
-  char *first_digit = find_first_digit(it, end);
-  char *first_non_digit = find_first_non_digit(it, end);
-  const size_t shift = first_non_digit - first_digit;
-  for (size_t i{}; i < shift; ++i) {
-    it = begin;
-    while (*(it + 1) != '\0') {
-      swap(it, it + 1);
-      ++it;
-    }
+  char *first_digit = find_first_occurence(it, end, [] (char c) { return c >= '0' && c <= '9'; });
+  char *first_non_digit = find_first_occurence(it, end, [] (char c) { return c < '0' || c > '9'; });
+  const ptrdiff_t SHIFT = first_non_digit - first_digit;
+  char *digits = new char[SHIFT];
+  for(ptrdiff_t i{0}; i < SHIFT; ++i) {
+    *(digits + i) = *(first_digit + i);
+  }
+  it = begin;
+  while(it != end - SHIFT) {
+    *it = *(it + SHIFT);
+    ++it;
+  }
+  while(it != end) {
+    *it++ = *digits++;
   }
 }
 
@@ -267,14 +257,18 @@ void reverse(char *begin, char *terminator) {
     std::cout << "reverse() - Exception\n";
     return;
   }
+  if (begin > terminator) {
+    std::cout << "reverse() - Exception\n";
+    return;
+  }
   const size_t SEQUENCE_HALF = (terminator - begin) / 2;
   for (size_t i{0}; i < SEQUENCE_HALF; ++i) {
-    swap(begin + i, terminator - 1 - i);
+    swap(*(begin + i), *(terminator - 1 - i));
   }
 }
 
 // sorts a char sequence according to the bubble sort algorithm
-void bubble_sort(char *begin, char *end, comp predicate) {
+void bubble_sort(char *begin, char *end, Comp predicate) {
   if (begin == nullptr || end == nullptr) {
     std::cout << "bubble_sort() - Exception\n";
     return;
@@ -290,7 +284,7 @@ void bubble_sort(char *begin, char *end, comp predicate) {
     size_t swap_counter{0};
     while (*(it + 1) != '\0') {
       if (predicate(*it, *(it + 1))) {
-        swap(it, it + 1);
+        swap(*it, *(it + 1));
         ++swap_counter;
       }
       ++it;
@@ -305,12 +299,12 @@ void bubble_sort(char *begin, char *end, comp predicate) {
   }
   // increasing order implementation
   if (*(terminator - 1) > *begin) {
-    shift(begin, end);
+    shift(begin, terminator);
   }
   // decreasing order implementation
   else {
     reverse(begin, terminator);
-    shift(begin, end);
+    shift(begin, terminator);
     reverse(begin, terminator);
   }
 }
@@ -328,7 +322,7 @@ std::function<int *(int *, int *)> choose_pivot[SIZE]{
     [](int *begin, int *end) { return begin + random_number(end - begin); }};
 
 // sorts an integer sequence according to the quicksort algorithm
-void quick_sort(int *begin, int *end, comp predicate) {
+void quick_sort(int *begin, int *end, Comp predicate) {
   if (begin == nullptr || end == nullptr) {
     std::cout << "quick_sort() - Exception\n";
     return;
@@ -338,7 +332,7 @@ void quick_sort(int *begin, int *end, comp predicate) {
   }
   // Choose reference pivot, then move it to the end
   int *pivot = choose_pivot[FIRST](begin, end);
-  swap(pivot, end - 1);
+  swap(*pivot, *(end - 1));
   pivot = end - 1;
   // it - general iterator, it2 - set to first number >= pivot. (<= for decreasing order)
   int *it{begin};
@@ -353,13 +347,13 @@ void quick_sort(int *begin, int *end, comp predicate) {
   // Swap it2 with any number < pivot, then incremenet it2. (> for decreasing order)
   while (it != end) {
     if (!predicate(*it, *pivot)) {
-      swap(it, it2);
+      swap(*it, *it2);
       ++it2;
     }
     ++it;
   }
   // Move pivot from end to proper location
-  swap(pivot, it2);
+  swap(*pivot, *it2);
   pivot = it2;
   // Repeat process for parts before and after the pivot
   quick_sort(begin, pivot, predicate);
@@ -403,6 +397,8 @@ void task_1() {
   const size_t ARRAY_SIZE{10}, NUMBER_OF_TESTS{1};
   const int LOW_LIMIT{-10}, HIGH_LIMIT{10};
   int array[ARRAY_SIZE]{}, array_copy[ARRAY_SIZE]{};
+  int* const array_end = array + ARRAY_SIZE;
+  int* const array_copy_end = array_copy + ARRAY_SIZE;
   std::chrono::duration<double> bubble_sort_duration{}, quick_sort_duration{};
 
   // arrange user settings and provide general info
@@ -417,40 +413,40 @@ void task_1() {
 
     // Fill array. Copy values to array_copy, so both algorithms process same data.
     if (RANDOM_ARRAY) {
-      fill_sequence_with_random_numbers(array, array + ARRAY_SIZE, LOW_LIMIT, HIGH_LIMIT);
+      fill_sequence_with_random_numbers(array, array_end, LOW_LIMIT, HIGH_LIMIT);
     } else {
-      fill_sequence_with_user_input(array, array + ARRAY_SIZE);
+      fill_sequence_with_user_input(array, array_end);
     }
-    copy_sequence(array, array + ARRAY_SIZE, array_copy);
+    copy_sequence(array, array_end, array_copy);
 
     // Show arrays before sorting
     if (ENABLE_PRINT) {
       std::cout << "------------------------------------\n";
       std::cout << "\t\t\tTEST " << i + 1 << std::endl;
       std::cout << "ARRAY BEFORE BUBBLESORT: ";
-      print_sequence(array, array + ARRAY_SIZE);
+      print_sequence(array, array_end);
       std::cout << "ARRAY BEFORE QUICKSORT : ";
-      print_sequence(array_copy, array_copy + ARRAY_SIZE);
+      print_sequence(array_copy, array_copy_end);
     }
 
     // Bubble sort duration
     auto bubble_start_time = std::chrono::system_clock::now();
-    bubble_sort(array, array + ARRAY_SIZE, [](int p1, int p2) { return p1 > p2; });
+    bubble_sort(array, array_end, [](int p1, int p2) { return p1 > p2; });
     auto bubble_end_time = std::chrono::system_clock::now();
     bubble_sort_duration += bubble_end_time - bubble_start_time;
 
     // Quick sort duration
     auto quick_start_time = std::chrono::system_clock::now();
-    quick_sort(array_copy, array_copy + ARRAY_SIZE, [](int p1, int p2) { return p1 >= p2; });
+    quick_sort(array_copy, array_copy_end, [](int p1, int p2) { return p1 >= p2; });
     auto quick_end_time = std::chrono::system_clock::now();
     quick_sort_duration += quick_end_time - quick_start_time;
 
     // Show arrays after sorting
     if (ENABLE_PRINT) {
       std::cout << "ARRAY AFTER BUBBLESORT : ";
-      print_sequence(array, array + ARRAY_SIZE);
+      print_sequence(array, array_end);
       std::cout << "ARRAY AFTER QUICKSORT  : ";
-      print_sequence(array_copy, array_copy + ARRAY_SIZE);
+      print_sequence(array_copy, array_copy_end);
     }
   }
   bubble_sort_duration /= NUMBER_OF_TESTS;
@@ -471,7 +467,7 @@ void task_1() {
 /************************************************* TASK 2 **********************************************************/
 
 // generates an unsigned array. shows it before and after the sorting process
-void print_and_sort_unsigned(comp predicate) {
+void print_and_sort_unsigned(Comp predicate) {
   const size_t UNSIGNED_ARRAY_SIZE{10};
   unsigned unsigned_array[UNSIGNED_ARRAY_SIZE]{1,  0, 300, 522, 6, 41, 0, 10,  155, 2222};
   unsigned *begin = unsigned_array;
@@ -485,7 +481,7 @@ void print_and_sort_unsigned(comp predicate) {
 }
 
 // generates a double array. shows it before and after the sorting process
-void print_and_sort_double(comp_double predicate) {
+void print_and_sort_double(CompDouble predicate) {
   const size_t DOUBLE_ARRAY_SIZE{5};
   double double_array[DOUBLE_ARRAY_SIZE]{-4.75, 5.11, 7.45, -4, 0};
   double *begin = double_array;
@@ -499,16 +495,16 @@ void print_and_sort_double(comp_double predicate) {
 }
 
 // generates a char array. shows it before and after the sorting process
-void print_and_sort_char(comp predicate) {
+void print_and_sort_char(Comp predicate) {
   const size_t CHAR_ARRAY_SIZE{1000};
   char char_array[CHAR_ARRAY_SIZE] = "zA1xB2yC3";
   char *begin = char_array;
   char *end = char_array + CHAR_ARRAY_SIZE;
   std::cout << "char array before sort: ";
-  print_sequence(begin, end);
+  print_sequence(begin);
   bubble_sort(begin, end, predicate);
   std::cout << "char array after sort : ";
-  print_sequence(begin, end);
+  print_sequence(begin);
 }
 
 // Task 2 - Sorting unsigned, double, and char arrays
@@ -518,15 +514,15 @@ void task_2() {
   std::cout << "------------------------------------\n";
   print_and_sort_unsigned([](unsigned p1, unsigned p2) { return p1 < p2; });
   print_and_sort_double([](double p1, double p2) { return p1 < p2; });
-  print_and_sort_char([](char p1, char p2) { return p1 < p2; });
+  print_and_sort_char([](char p1, char p2) { return p1 > p2; });
 }
 
 /*********************************************** MENU *************************************************/
 
-enum MENU { QUIT, TASK_1, TASK_2, DEFAULT };
+enum MENU { QUIT, TASK_1, TASK_2};
 
 int main() {
-  std::function<void()> menu[DEFAULT]{task_1, task_2};
+  std::function<void()> menu[TASK_2]{task_1, task_2};
   while (true) {
     std::cout << "------------------------------------\n";
     std::cout << "               MENU                 \n";
