616403af21f0e2a429dea0cd104b7dd041844cb9
Amatory1107 <58076512+Amatory1107@users.noreply.github.com>
2020-01-04 11:10:01 +0200

    Update Igor_vyhovanets_Second_hometaskcpp4.cpp

--- HomeWork2/Igor_vyhovanets_Second_hometaskcpp4.cpp
+++ HomeWork2/Igor_vyhovanets_Second_hometaskcpp4.cpp
@@ -4,16 +4,16 @@ int main() {
   int sum = 0;
   int number_of_digits = 1;
   size_t max_iteration = {50};
-  std::operator<<(
+  std::cout<<(
       std::cout,
       "This program calculates the sum of odd numbers in the range \n");
-  std::operator<<(std::cout,
+  std::cout<<(std::cout,
                   "from -60 to 90, and you can enter no more than 50 numbers.")
       .
       operator<<(std::endl);
   while (max_iteration > 0) {
     int number;
-    std::operator<<(std::operator<<(std::cout, "Enter number ")
+    std::cout<<(std::operator<<(std::cout, "Enter number ")
                         .
                         operator<<(number_of_digits),
                     ":")
@@ -27,12 +27,11 @@ int main() {
         number = 0;
       }
 
-      ;
-      ;
+    
       ++number_of_digits;
       --max_iteration;
     } else {
-      std::operator<<(std::operator<<(std::cout, "Enter number ")
+      std::cout<<(std::operator<<(std::cout, "Enter number ")
                           .
                           operator<<(number_of_digits),
                       ":")
@@ -42,6 +41,6 @@ int main() {
     }
   }
 
-  std::operator<<(std::cout, "Sum of odd elements- ").operator<<(sum);
+  std::cout<<(std::cout, "Sum of odd elements- ").operator<<(sum);
   return 0;
 }
