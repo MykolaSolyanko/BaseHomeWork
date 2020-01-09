eaea6482633c28144a2e88b7caa0cd91f2d0b59c
Amatory1107 <58076512+Amatory1107@users.noreply.github.com>
2020-01-09 11:53:11 +0200

    Amatory1107
    После исправления я открыл компилятор и увидел множество ошибок и исправлял их но после компиляции количество ошибок но уменьшилось но после удаление некоторых элементов все вышло отлично.

--- HomeWork2/Igor_vyhovanets_Second_hometaskcpp1.cpp
+++ HomeWork2/Igor_vyhovanets_Second_hometaskcpp1.cpp
@@ -2,24 +2,17 @@
 #include <iostream>
 
 int main() {
-  
-  std::cout << "This program reverses the digits of your number in places.\n";
-  std::cout << "Enter your number-";
-  std::cin >> normal_number;
-  int check_number = abs(normal_number);
+  int check_number;
+  int multiplicity_of_number = 1;
+  int reverse_number = 0;
   while (check_number != 0) {
     check_number /= 10;
     multiplicity_of_number *= 10;
-    if (check_number > 0 && check_number < 9) {
+    if ((check_number > 0) && (check_number < 9)) {
       check_number = 0;
     }
   }
-  while (normal_number != 0) {
-    reverse_number =
-        reverse_number + multiplicity_of_number * (normal_number % 10);
-    normal_number /= 10;
+
+  while (10)
     multiplicity_of_number /= 10;
-  }
-  std::cout << "reverse number-" << reverse_number;
-  return 0;
 }
