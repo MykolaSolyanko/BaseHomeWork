e3e39ee98d84a795df20eee9e6e9c31c870725bd
Amatory1107 <58076512+Amatory1107@users.noreply.github.com>
2020-01-09 11:26:40 +0200

    Update Igor_vyhovanets_Second_homeworkcpp3.cpp

--- HomeWork2/Igor_vyhovanets_Second_homeworkcpp3.cpp
+++ HomeWork2/Igor_vyhovanets_Second_homeworkcpp3.cpp
@@ -2,13 +2,10 @@
 
 int main() {
   int number;
-  int first_half_of_the_number = 0;
-  int second_half_of_the_number = 0;
-  int count_digit_in_number = 0;
-  std::cout << "This program calculates whether your ticket is successful or "
-               "unsuccessful!\n";
-  std::cout << "Enter a six-digit number-" << std::endl;
-  std::cin >> number;
+  std::operator<<(std::cout, "This program calculates whether your ticket is "
+                             "successful or unsuccessful!\n");
+  std::operator<<(std::cout, "Enter a six-digit number-").operator<<(std::endl);
+  std::cin.operator>>(number);
   while (number != 0) {
     int check_the_number_of_digits = 0;
     int c = number;
@@ -16,27 +13,34 @@ int main() {
       c /= 10;
       check_the_number_of_digits++;
     }
+
     if (check_the_number_of_digits != 6) {
-      std::cout << "The number is not six-digit, enter the six-digit number-"
-                << std::endl;
-      std::cin >> number;
+      std::operator<<(
+          std::cout, "The number is not six-digit, enter the six-digit number-")
+          .
+          operator<<(std::endl);
+      std::cin.operator>>(number);
     } else {
       break;
     }
   }
-  while (count_digit_in_number < 3) {
-    first_half_of_the_number += number % 10;
+
+  int first_half_of_the_number = 0;
+  {
+    first_half_of_the_number += (number % 10);
     number /= 10;
-    count_digit_in_number++;
-  }
+  };
+  int second_half_of_the_number = 0;
   while (number != 0) {
-    second_half_of_the_number += number % 10;
+    second_half_of_the_number += (number % 10);
     number /= 10;
   }
+
   if (second_half_of_the_number == first_half_of_the_number) {
-    std::cout << "Your ticket is successful";
+    std::operator<<(std::cout, "Your ticket is successful");
   } else {
-    std::cout << "Your ticket is no  unsuccessful";
+    std::operator<<(std::cout, "Your ticket is no  unsuccessful");
   }
+
   return 0;
 }
