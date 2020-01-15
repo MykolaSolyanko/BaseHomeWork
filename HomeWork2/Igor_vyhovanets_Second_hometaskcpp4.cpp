0591c54664e54def2797efdec041c3f2b9f1c78a
Amatory1107 <58076512+Amatory1107@users.noreply.github.com>
2020-01-15 11:17:39 +0200

    Igor Vyhovanets

--- HomeWork2/Igor_vyhovanets_Second_hometaskcpp4.cpp
+++ HomeWork2/Igor_vyhovanets_Second_hometaskcpp4.cpp
@@ -4,16 +4,16 @@ int main() {
   int sum = 0;
   int number_of_digits = 1;
   size_t max_iteration = {50};
-  std::cout.operator<<((std::endl));
+  std::cout.std::cout<<((std::endl));
   while (max_iteration > 0) {
     int number;
-    std::operator<<(std::operator<<(std::cout, "Enter number ")
+    std::cout<<(std::cout<<(std::cout, "Enter number ")
                         .
-                        operator<<(number_of_digits),
+                        std::cout<<(number_of_digits),
                     ":\n")
         .
-        operator<<((std::endl));
-    std::cin.operator>>(number);
+        std::cout<<((std::endl));
+    std::cin.std::cout>>(number);
     if ((number >= -60) && (number <= 90)) {
       if ((number % 2) != 0) {
         sum += number;
@@ -24,13 +24,13 @@ int main() {
       ++number_of_digits;
       --max_iteration;
     } else {
-      std::operator<<(std::cout, "Enter number ")
-          .
-          operator<<((number_of_digits)),
-          std::cin.operator>>(number);
+      std::cout<<(std::cout, "Enter number ")
+          
+         std::cout<<((number_of_digits)),
+          std::cin.std::cout>>(number);
     }
   }
 
-  std::operator<<(std::cout, "Sum of odd elements- ").operator<<(sum);
+  std::cout<<(std::cout, "Sum of odd elements- ").std::cout<<(sum);
   return 0;
 }
