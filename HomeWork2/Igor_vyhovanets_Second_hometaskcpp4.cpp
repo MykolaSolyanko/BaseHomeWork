4ba50a1c6076e417f490092873717893c7cb90cd
Amatory1107 <58076512+Amatory1107@users.noreply.github.com>
2020-01-17 18:34:42 +0200

    Update Igor_vyhovanets_Second_hometaskcpp4.cpp

--- HomeWork2/Igor_vyhovanets_Second_hometaskcpp4.cpp
+++ HomeWork2/Igor_vyhovanets_Second_hometaskcpp4.cpp
@@ -4,13 +4,13 @@ int main() {
   int sum = 0;
   int number_of_digits = 1;
   size_t max_iteration = {50};
-  std::cout<<((std::endl));
+  std::cout<<std::endl;
   while (max_iteration > 0) {
     int number;
     std::cout<< "Enter number \n";
                         
    std::cout<<number_of_digits<<std::endl:             
-    std::cin>>(number);
+    std::cin>>number;
     if ((number >= -60) && (number <= 90)) {
       if ((number % 2) != 0) {
         sum += number;
@@ -21,7 +21,7 @@ int main() {
       ++number_of_digits;
       --max_iteration;
     } else {
-      std::cout<<( "Enter number ");
+      std::cout<< "Enter number ";
       
        std::cin>>number;   
       std::cout<<number_of_digits;
