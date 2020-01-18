34466bf3acd65a67a6ecec46ef1bcab92aecda3c
Amatory1107 <58076512+Amatory1107@users.noreply.github.com>
2020-01-18 11:26:48 +0200

    Amatory1107
    Third home task

---
+++ HomeWork3/Third_workcpp.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+ #include <cstring>
+
+ int main(int argc, char *argv[]) {
+
+   std::cout << "This program transforms string to lower case" << std::endl;
+
+   const size_t kMaxLength = 100;
+   char inputString[kMaxLength]{}, outputString[kMaxLength]{};
+
+   std::cout << "Enter a string (max " << kMaxLength << " symbols): ";
+   std::cin >> inputString;
+
+   size_t length = strlen(inputString);
+
+   if (length > kMaxLength) {
+     std::cout << "Error! Too long string. Exiting." << std::endl;
+     return 0;
+   }
+
+   std::cout << "Input string:  " << inputString << std::endl;
+
+   const char kDiff = 'A'-'a';
+
+   for (size_t i = 0; i < length; i++) {
+     outputString[i] = inputString[i];
+     if (inputString[i] >= 'A' && inputString[i] <= 'Z') {
+       outputString[i] -= kDiff;
+     }
+   }
+
+   std::cout << "Output string: " << outputString << std::endl;
+
+   return 0;
+ }
