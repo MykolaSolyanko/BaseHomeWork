801ce9f1c631785a770f4f6e4c51dd59f0176594
Amatory1107 <58076512+Amatory1107@users.noreply.github.com>
2020-01-20 11:05:11 +0200

    Amatory1107
    1-6 hometask 3

---
+++ HomeWork3/Igor Vyhovanets Third Hometask 1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+
+int main(int argc, char* argv[]) {
+  std::cout << "This program reverses string that user inputs" << std::endl;
+
+  if (argc != 2) {
+    std::cout << "Wrong number of parameters" << std::endl;
+    std::cout << "Usage: ./reverse <string>" << std::endl;
+    return 0;
+  }
+  
+  const size_t kMaxLength = 100;
+  const size_t kLength = strlen(argv[1]);
+
+  if (kLength > kMaxLength) {
+    std::cout << "Error! Input string should be less than " << kMaxLength << " characters" << std::endl;
+    return 0;
+  }
+
+  char reversed[kMaxLength + 1]{};  // string should contain termination zero (+1 byte)
+  char *pReversed = reversed;    
+
+  // Reverse the string
+  for (auto *begin = argv[1], *end = begin + kLength, *limit = end; begin < limit; ++begin) {
+    *pReversed++ = *--end;
+  }
+
+  std::cout << "Initial string:  " << argv[1] << std::endl;
+  std::cout << "Reversed string: " << reversed << std::endl;
+  
+  return 0;
